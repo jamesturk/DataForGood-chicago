59b879db2b466f1a06cddc31741041d351c390e0
Yueyue Wang <yueyue@linux6.cs.uchicago.edu>
2024-05-20 15:07:36 -0500

    testmainchartviz in tests.py

--- dataforgood/main/tests.py
+++ dataforgood/main/tests.py
@@ -794,7 +794,6 @@ class TestMainChartViz(TestCase):
                 {'name': 'Series 3', 'data': [7, 8, 9]}
             ]
         }
-        
         table_title = 'Main Chart'
         
         template_string = '''
@@ -803,15 +802,64 @@ class TestMainChartViz(TestCase):
                 var chartData = {{ chart_data | safe }};
                 var currentView = 'category';
                 
-                function updateMainChart() {
-                    // ... (your existing code for updating the chart)
-                }
-                
-                updateMainChart();
-                
-                document.getElementById('toggle-main-view').addEventListener('click', function() {
-                    currentView = currentView === 'category' ? 'series' : 'category';
-                    updateMainChart();
+                        function updateMainChart() {
+                            var categories = chartData.categories;
+                            var series = chartData.series;
+
+                            var seriesData;
+
+                            if (currentView === 'category') {
+                                seriesData = series;
+                            } else {
+                                seriesData = categories.map(function(category, index) {
+                                    var categoryData = series.map(function(serie) {
+                                        return serie.data[index];
+                                    });
+
+                                    return {
+                                        name: category,
+                                        data: categoryData
+                                    };
+                                });
+                            }
+
+                            var chartOptions = {
+                                chart: {
+                                    type: 'bar'
+                                },
+                                title: {
+                                    text: '{{ table_title }}'
+                                },
+                                xAxis: {
+                                    categories: currentView === 'category' ? categories : series.map(function(serie) {
+                                        return serie.name;
+                                    })
+                                },
+                                yAxis: {
+                                    min: 0,
+                                    title: {
+                                        text: 'Value'
+                                    }
+                                },
+                                legend: {
+                                    reversed: true
+                                },
+                                plotOptions: {
+                                    series: {
+                                        stacking: 'normal'
+                                    }
+                                },
+                                series: seriesData
+                            };
+
+                            Highcharts.chart('chart-container', chartOptions);
+                        }
+
+                        updateMainChart();
+
+                        document.getElementById('toggle-main-view').addEventListener('click', function() {
+                            currentView = currentView === 'category' ? 'series' : 'category';
+                            updateMainChart();
                 });
             });
         </script>
